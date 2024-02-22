f913abe536bafbf91cc46e158e5942524758ef55
Ryan <139408696+ryannnc@users.noreply.github.com>
2024-02-22 17:27:24 -0500

    first commit for backend folder

---
+++ backend/DB/db.js
@@ -0,0 +1,16 @@
+const {trace} = require('console');
+const mongoose = require("mongoose");
+
+const db = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('Db connected');
+    } catch (error) {
+        console.log('Db connection error');
+        
+    }
+
+}
+
+module.exports = {db}
